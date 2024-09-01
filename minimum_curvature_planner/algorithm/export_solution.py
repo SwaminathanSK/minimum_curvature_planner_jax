a744d2446fecd4ac1eaf0988dbd52d2988aadf50
real-Sandip-Das <sd13535@outlook.com>
2024-09-01 18:17:49 +0530

    Added code for exporting solution to a json file

--- minimum_curvature_planner/algorithm/export_solution.py
+++ minimum_curvature_planner/algorithm/export_solution.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import numpy as np
 from perception_data import Centreline
 
@@ -8,5 +9,20 @@ def export_solution(points: np.ndarray, output_path: str, format: str = 'csv'):
             writer = csv.writer(file)
             for point in points:
                 writer.writerow(point)
+    elif format == 'json':
+        json_data = [
+            {
+                'pose': {
+                    'position': {
+                        'x': point[0],
+                        'y': point[1],
+                        'z': 0.0
+                    }
+                }
+            }
+            for point in points
+        ]
+        with open(output_path, mode='w') as file:
+            json.dump(json_data, file, indent=4)
     else:
         raise ValueError(f"Unsupported format: {format}")
