71a171b6c2815060f82fd7d387d697eec9e0e2a7
Swaminathan S K <swamisathya2004@gmail.com>
2024-10-01 04:14:24 +0530

    3_deg visualized by 2_deg

--- minimum_curvature_planner/algorithm/matrices.py
+++ minimum_curvature_planner/algorithm/matrices.py
@@ -100,7 +100,6 @@ def M_comp(centreline, component: np.int32):
 def first_derivatives(centreline: Centreline, Ainv: np.ndarray, q: np.ndarray):
     A_ex_b = A_ex_comp(centreline.N, 1)
     return A_ex_b @ Ainv @ q
-import numpy as np
 
 def matPxx(x_dashed: np.ndarray, y_dashed: np.ndarray):
     values = y_dashed**2 / (x_dashed**2 + y_dashed**2)**3
@@ -127,6 +126,114 @@ def matrices_H_f(centreline: Centreline):
 
     M_x = M_comp(centreline, 0)
     M_y = M_comp(centreline, 1)
+
+    T_c = 2 * A_ex_c @ Ainv
+    T_n_x = T_c @ M_x
+    T_n_y = T_c @ M_y
+    
+    P_xx = matPxx(x_d, y_d)
+    P_xy = matPxy(x_d, y_d)
+    P_yy = matPyy(x_d, y_d)
+
+    H_x = T_n_x.T @ P_xx @ T_n_x
+    H_xy = T_n_y.T @ P_xy @ T_n_x
+    H_y = T_n_y.T @ P_yy @ T_n_y
+    H = 2*(H_x + H_xy + H_y)
+
+    f_x = 2 * T_n_x.T @ P_xx.T @ T_c @ q_x
+    f_xy = T_n_y.T @ P_xy.T @ T_c @ q_x + T_n_x.T @ P_xy.T @ T_c @ q_y
+    f_y = 2 * T_n_y.T @ P_yy.T @ T_c @ q_y
+    f = f_x + f_xy + f_y
+    
+    return H, f
+
+def matAInv_deg2(N: np.int32):
+    A = np.zeros((3*N, 3*N))
+    
+    # Fill the matrix using vectorized operations
+    indices = np.arange(0, 3*N, 3)
+    A[indices, indices] = 1
+    
+    indices = np.arange(1, 3*N, 3)
+    A[indices, indices-1] = 1
+    A[indices, indices] = 1
+    A[indices, indices+1] = 1
+    # A[indices, indices+2] = 1
+    
+    indices = np.arange(2, 3*N, 3)
+    addr_A_N_1 = (indices + 3*N - 5) % (3*N)
+    A[indices, indices-1] = -1
+    A[indices, addr_A_N_1 + 1] = 1
+    A[indices, addr_A_N_1 + 2] = 2
+    # A[indices, addr_A_N_1 + 3] = 3
+    
+    # indices = np.arange(2, 3*N, 3)
+    # addr_A_N_1 = (indices + 3*N - 5) % (3*N)
+    # A[indices, indices-1] = -2
+    # A[indices, addr_A_N_1 + 2] = 2
+    # # A[indices, addr_A_N_1 + 3] = 6
+    
+    A_inv = np.linalg.inv(A)
+    A_inv[np.isclose(A_inv, 0, atol=1e-15)] = 0
+    return A_inv
+
+def A_ex_comp_deg2(N: np.int32, component: np.int32):
+    # A_ex = np.zeros((N, 4*N), dtype=np.float64)
+    A_ex = np.zeros((N, 3*N), dtype=np.float64)
+
+    indices = np.arange(N)
+    # A_ex[indices, 4*indices + component] = 1
+    A_ex[indices, 3*indices + component] = 1
+
+    return A_ex
+
+
+def q_comp_deg2(centreline, component: np.int32):
+    N = centreline.N
+    # q = np.zeros(4 * N, dtype=np.float64)
+    q = np.zeros(3 * N, dtype=np.float64)
+
+    indices = np.arange(N)
+    # q[4 * indices] = centreline.p[indices, component]
+    # q[4 * indices + 1] = centreline.p[(indices + 1) % N, component]
+
+
+    q[3 * indices] = centreline.p[indices, component]
+    q[3 * indices + 1] = centreline.p[(indices + 1) % N, component]
+    
+    return q
+
+
+def M_comp_deg2(centreline, component: np.int32):
+    N = centreline.N
+    # M = np.zeros((4 * N, N), dtype=np.float64)
+    M = np.zeros((3 * N, N), dtype=np.float64)
+
+    indices = np.arange(N)
+    # M[4 * indices, indices] = centreline.n[indices, component]
+    # M[4 * indices + 1, (indices + 1) % N] = centreline.n[(indices + 1) % N, component]
+    M[3 * indices, indices] = centreline.n[indices, component]
+    M[3 * indices + 1, (indices + 1) % N] = centreline.n[(indices + 1) % N, component]
+    
+    return M
+
+def first_derivatives_deg2(centreline: Centreline, Ainv: np.ndarray, q: np.ndarray):
+    A_ex_b = A_ex_comp_deg2(centreline.N, 1)
+    return A_ex_b @ Ainv @ q
+
+def matrices_H_f_deg2(centreline: Centreline):
+    # returns a tuple of the matrices H and f that define the QP
+    Ainv = matAInv_deg2(centreline.N)
+    A_ex_c = A_ex_comp_deg2(centreline.N, 2)
+    q_x = q_comp_deg2(centreline, 0)
+    q_y = q_comp_deg2(centreline, 1)
+    x_d = first_derivatives_deg2(centreline, Ainv, q_x) # vector containing x_i'
+    y_d = first_derivatives_deg2(centreline, Ainv, q_y) # vector containing y_i'
+
+    centreline.calc_n(x_d, y_d)
+
+    M_x = M_comp_deg2(centreline, 0)
+    M_y = M_comp_deg2(centreline, 1)
 
     T_c = 2 * A_ex_c @ Ainv
     T_n_x = T_c @ M_x
